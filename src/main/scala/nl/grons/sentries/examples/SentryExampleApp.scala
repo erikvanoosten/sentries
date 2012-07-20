abccd37db18df7be3d861fd946a4aa45ee32506b
Erik van Oosten <evanoosten@ebay.com>
2012-07-20 21:18:23 +0200

    Switched to BSD license.

--- src/main/scala/nl/grons/sentries/examples/SentryExampleApp.scala
+++ src/main/scala/nl/grons/sentries/examples/SentryExampleApp.scala
@@ -1,17 +1,11 @@
 /*
- * Copyright (c) 2012 Erik van Oosten
+ * Sentries
+ * Copyright (c) 2012. Erik van Oosten. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * The primary distribution site is https://github.com/erikvanoosten/sentries
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This software is released under the terms of the Revised BSD License.
+ * There is NO WARRANTY. See the file LICENSE for the full text.
  */
 
 package examples
--- src/main/scala/nl/grons/sentries/support/SentriesRegistryListener.scala
+++ src/main/scala/nl/grons/sentries/support/SentriesRegistryListener.scala
@@ -1,3 +1,13 @@
+/*
+ * Sentries
+ * Copyright (c) 2012. Erik van Oosten. All rights reserved.
+ *
+ * The primary distribution site is https://github.com/erikvanoosten/sentries
+ *
+ * This software is released under the terms of the Revised BSD License.
+ * There is NO WARRANTY. See the file LICENSE for the full text.
+ */
+
 package nl.grons.sentries.support
 
 import java.util.EventListener
