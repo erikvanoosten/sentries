d8ee73f5f0213f5fb1db11478f00592d04c656b9
Erik van Oosten <evanoosten@ebay.com>
2012-07-21 21:13:18 +0200

    Added warning and commented out some experimental stuff in LoadBalancer.

--- src/main/scala/nl/grons/sentries/core/LoadBalancer.scala
+++ src/main/scala/nl/grons/sentries/core/LoadBalancer.scala
@@ -10,13 +10,16 @@
 
 package nl.grons.sentries.core
 
-import java.util.concurrent.atomic.{AtomicLong, AtomicReference}
 import nl.grons.sentries.support.{NotAvailableException, NamedSentry}
 import scala.util.Random
 import scala.annotation.tailrec
 
 /**
  * Does load balancing between several resources.
+ *
+ * Warning: the API is definitely not yet stable.
+ * When used wrong, you cause a memory leak.
+ * Use at your own risk.
  *
  * @param loadBalancerName name of the load balancer
  * @param reporter call back function invoked when a resource failed but more resources can be tried,
@@ -55,59 +58,61 @@ abstract class LoadBalancer[R](loadBalancerName: String, val reporter: (String, 
   }
 }
 
-// TODO: document initialization order in case cacheDuration is overridden
-trait Caching[R] extends LoadBalancer[R] {
-  val cacheDuration = 5000L
-  private[this] val cache = new AtomicReference(super.resources)
-  private[this] val nextCacheExpiration = new AtomicLong(System.currentTimeMillis() + cacheDuration)
+// Please don't use this yet.
+//// TODO: document initialization order in case cacheDuration is overridden
+//trait Caching[R] extends LoadBalancer[R] {
+//  val cacheDuration = 5000L
+//  private[this] val cache = new AtomicReference(super.resources)
+//  private[this] val nextCacheExpiration = new AtomicLong(System.currentTimeMillis() + cacheDuration)
+//
+//  override abstract def resources: IndexedSeq[(R, NamedSentry)] = {
+//    val now: Long = System.currentTimeMillis()
+//    val nextExp: Long = nextCacheExpiration.get
+//    if (now >= nextExp && nextCacheExpiration.compareAndSet(nextExp, now + cacheDuration)) {
+//      val updatedResources = super.resources
+//      cache.set(updatedResources)
+//      updatedResources
+//    } else {
+//      cache.get
+//    }
+//  }
+//}
 
-  override abstract def resources: IndexedSeq[(R, NamedSentry)] = {
-    val now: Long = System.currentTimeMillis()
-    val nextExp: Long = nextCacheExpiration.get
-    if (now >= nextExp && nextCacheExpiration.compareAndSet(nextExp, now + cacheDuration)) {
-      val updatedResources = super.resources
-      cache.set(updatedResources)
-      updatedResources
-    } else {
-      cache.get
-    }
-  }
-}
-
-// TODO: document initialization order in case initialCacheDuration is overridden
-trait VariableCaching[R] extends LoadBalancer[R] {
-  val initialCacheDuration = 5000L
-  val minimumCacheDuration = 100L
-  private[this] val cache = new AtomicReference(super.resources)
-  private[this] val nextCacheExpiration = new AtomicLong(System.currentTimeMillis() + initialCacheDuration)
-  private[this] val currentCacheDuration = new AtomicLong(initialCacheDuration)
-
-  override abstract def resources: IndexedSeq[(R, NamedSentry)] = {
-    val now: Long = System.currentTimeMillis()
-    val nextExp: Long = nextCacheExpiration.get
-    if (now >= nextExp && nextCacheExpiration.compareAndSet(nextExp, now + currentCacheDuration.get)) {
-      val updatedResources = super.resources
-      cache.set(updatedResources)
-      updatedResources
-    } else {
-      cache.get
-    }
-  }
-
-  override abstract def apply[T](r: R => T): T = {
-    try {
-      val t = super.apply(r)
-      currentCacheDuration.set(initialCacheDuration)
-      t
-    } catch {
-      case e: NotAvailableException =>
-        val c = currentCacheDuration.get
-        val updated = ((c * 2) / 3).max(minimumCacheDuration)
-        if (c != updated) currentCacheDuration.compareAndSet(c, updated)
-        throw e
-    }
-  }
-}
+// Please don't use this yet.
+//// TODO: document initialization order in case initialCacheDuration is overridden
+//trait VariableCaching[R] extends LoadBalancer[R] {
+//  val initialCacheDuration = 5000L
+//  val minimumCacheDuration = 100L
+//  private[this] val cache = new AtomicReference(super.resources)
+//  private[this] val nextCacheExpiration = new AtomicLong(System.currentTimeMillis() + initialCacheDuration)
+//  private[this] val currentCacheDuration = new AtomicLong(initialCacheDuration)
+//
+//  override abstract def resources: IndexedSeq[(R, NamedSentry)] = {
+//    val now: Long = System.currentTimeMillis()
+//    val nextExp: Long = nextCacheExpiration.get
+//    if (now >= nextExp && nextCacheExpiration.compareAndSet(nextExp, now + currentCacheDuration.get)) {
+//      val updatedResources = super.resources
+//      cache.set(updatedResources)
+//      updatedResources
+//    } else {
+//      cache.get
+//    }
+//  }
+//
+//  override abstract def apply[T](r: R => T): T = {
+//    try {
+//      val t = super.apply(r)
+//      currentCacheDuration.set(initialCacheDuration)
+//      t
+//    } catch {
+//      case e: NotAvailableException =>
+//        val c = currentCacheDuration.get
+//        val updated = ((c * 2) / 3).max(minimumCacheDuration)
+//        if (c != updated) currentCacheDuration.compareAndSet(c, updated)
+//        throw e
+//    }
+//  }
+//}
 
 /**
  * No resources present at all, invocations are failing immediately.
