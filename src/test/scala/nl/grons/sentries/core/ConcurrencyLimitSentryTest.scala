409dfb3b4756f8fdd9511f8d2c4623fa784e8e73
Erik van Oosten <evanoosten@ebay.com>
2012-07-22 21:38:57 +0200

    Added ConcurrencyLimitSentryTest.

---
+++ src/test/scala/nl/grons/sentries/core/ConcurrencyLimitSentryTest.scala
@@ -0,0 +1,79 @@
+/*
+ * Sentries
+ * Copyright (c) 2012 Erik van Oosten All rights reserved.
+ *
+ * The primary distribution site is https://github.com/erikvanoosten/sentries
+ *
+ * This software is released under the terms of the BSD 2-Clause License.
+ * There is NO WARRANTY. See the file LICENSE for the full text.
+ */
+
+package nl.grons.sentries.core
+
+import org.specs2.mutable.Specification
+import nl.grons.sentries.support.NotAvailableException
+import org.specs2.specification.Scope
+import java.util.concurrent.{ExecutionException, Future, Callable, Executors}
+import scala.collection.JavaConverters._
+
+/**
+ * Tests {@link ConcurrencyLimitSentry}.
+ */
+class ConcurrencyLimitSentryTest extends Specification {
+
+  "The concurrency limit sentry" should {
+    "return value" in new SentryContext {
+      sentry("value") must_== "value"
+    }
+
+    "rethrow exception" in new SentryContext {
+      sentry(throwAnIllegalArgumentException) must throwA[IllegalArgumentException]
+    }
+
+    "throw NotAvailableException for too many invocations" in new SentryContext {
+      val executor = Executors.newFixedThreadPool(10)
+      val options = try {
+        val task = new Callable[String] {
+          def call() = sentry(slowCode)
+        }
+        val tasks = Vector.fill(10)(task).asJava
+
+        // Start all 10 tasks simultaneously:
+        val futures = executor.invokeAll(tasks).asScala
+        futuresToOptions(futures)
+
+      } finally {
+        executor.shutdown()
+      }
+
+      options.filter(_ == Some("slow")).size must_== 4
+      options.filter(_ == None).size must_== 6
+    }
+
+    "this line keeps intellij happy" in { todo }
+  }
+
+  trait SentryContext extends Scope {
+    val sentry = new ConcurrencyLimitSentry("testSentry", 4, classOf[ConcurrencyLimitSentryTest])
+
+    def slowCode = {
+      Thread.sleep(20L)
+      "slow"
+    }
+
+    def throwAnIllegalArgumentException: String = {
+      throw new IllegalArgumentException("fail")
+    }
+
+    // Convert normal Future results to a Some, and NotAvailableExceptions
+    // (wrapped in ExecutionException) to a None.
+    def futuresToOptions[A](futures: Seq[Future[A]]): Seq[Option[A]] = futures.map { future =>
+      try Some(future.get())
+      catch {
+        case e: ExecutionException if e.getCause.isInstanceOf[NotAvailableException] => None
+        case e => throw e
+      }
+    }
+  }
+
+}
